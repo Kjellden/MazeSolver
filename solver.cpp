79c7fef2eb46ea26139abaa988d45cdd31e600eb
kjellden <kjelldenknutzen@gmail.com>
2024-06-12 15:56:47 -0600

    Add a file that makes mazes and changed the main so that you user can pick what size maze they would like the game to solve

--- solver.cpp
+++ solver.cpp
@@ -1,6 +1,52 @@
 #include <iostream>
+#include <vector>
+#include <windows.h>
+
+#include "mazes.cpp"
 using namespace::std;
 
-int main(){
+// Function to display the maze
+void displayMaze(char** maze, int height, int width) {
+    for (int i = 0; i < height; ++i) {
+        for (int j = 0; j < width; ++j) {
+            cout << maze[i][j] << ' ';
+        }
+        cout << std::endl;
+    }
+}
+
+int main() {
+    // set vars
+    int mazeSelect;
+    char** maze;
+    int mazeHeight;
+    int mazeWidth;
+
+    // Display the maze
+    cout << "Welcome to the maze solver program what size maze would you like to solve?" << endl;
+    cout << "1: 10x10 maze" << endl;
+    cout << "2: 15x15 maze" << endl;
+    cout << "What option would you like: ";
+    cin >> mazeSelect;
+
+    // set the size of the maze and make the maze off user choice
+    if (mazeSelect == 1){
+        mazeHeight = 10;
+        mazeWidth = 10;
+        maze = maze_10x10();
+    }
+
+    else if (mazeSelect == 2){
+        mazeHeight = 15;
+        mazeWidth = 15;
+        maze = maze_15x15();
+    }
+    
+    // start displaying maze
+    std::cout << "\033[2J\033[1;1H";
+    displayMaze(maze, mazeHeight, mazeWidth);
+    Sleep(1000);
+    std::cout << "\033[2J\033[1;1H";
+    displayMaze(maze, mazeHeight, mazeWidth);
     return 0;
 }
