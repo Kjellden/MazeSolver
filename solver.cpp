9fdc49f69f8aac13aeba1c8ef40c05ca81e126cb
kjellden <kjelldenknutzen@gmail.com>
2024-06-12 18:03:28 -0600

    Added a DFS solver for the maze

--- solver.cpp
+++ solver.cpp
@@ -1,12 +1,14 @@
 #include <iostream>
 #include <vector>
 #include <windows.h>
-
+#include <list>
 #include "mazes.cpp"
 using namespace::std;
 
+
 // Function to display the maze
 void displayMaze(char** maze, int height, int width) {
+    std::cout << "\033[2J\033[1;1H";
     for (int i = 0; i < height; ++i) {
         for (int j = 0; j < width; ++j) {
             cout << maze[i][j] << ' ';
@@ -16,14 +18,61 @@ void displayMaze(char** maze, int height, int width) {
 }
 
 // Finds the starting postion of the maze this allows us to start moving down the maze this is down in O(n)
-pair<int, int> findStartPosition(char** maze, int rows, int cols) {
+std::pair<int, int> findStartPosition(char** maze, int rows, int cols) {
     for (int i = 0; i < rows; ++i) {
         for (int j = 0; j < cols; ++j) {
             if (maze[i][j] == 'S') {
-                return make_pair(i, j);
+                return std::make_pair(i, j);
             }
         }
     }
+    return std::make_pair(-1, -1); // Return an invalid position if 'S' is not found
+}
+
+// solver the maze using dfs
+bool dfsSolve(char** maze,int mazeHeight, int mazeWidth, pair<int,int> pos){
+    pair<int, int> move;
+    bool solved;
+    
+    displayMaze(maze, mazeHeight, mazeWidth);
+    Sleep(500);
+
+    if (maze[pos.first][pos.second] == 'E'){
+        return true;
+    }
+
+    maze[pos.first][pos.second] = '*';
+
+    if (maze[pos.first + 1][pos.second] == ' ' or maze[pos.first + 1][pos.second] == 'E'){
+        move = make_pair(pos.first + 1, pos.second);
+        if (dfsSolve(maze, mazeHeight, mazeWidth, move)){
+            return true;
+        }
+    }
+
+    if (maze[pos.first][pos.second + 1] == ' ' or maze[pos.first][pos.second + 1] == 'E'){
+        move = make_pair(pos.first, pos.second + 1);
+        if (dfsSolve(maze, mazeHeight, mazeWidth, move)){
+            return true;
+        }
+    }
+
+    if (maze[pos.first - 1][pos.second] == ' ' or maze[pos.first - 1][pos.second] == 'E'){
+        move = make_pair(pos.first - 1, pos.second);
+        if (dfsSolve(maze, mazeHeight, mazeWidth, move)){
+            return true;
+        }
+    }
+
+    if (maze[pos.first][pos.second -1] == ' ' or maze[pos.first][pos.second - 1] == 'E'){
+        move = make_pair(pos.first, pos.second - 1);
+        if (dfsSolve(maze, mazeHeight, mazeWidth, move)){
+            return true;
+        }
+    }
+
+    maze[pos.first][pos.second] = '!';
+    return false;
 }
 
 int main() {
@@ -53,12 +102,9 @@ int main() {
         maze = maze_15x15();
     }
      
-    Sleep(4000);
     // start displaying maze
     std::cout << "\033[2J\033[1;1H";
-    displayMaze(maze, mazeHeight, mazeWidth);
-    Sleep(1000);
-    std::cout << "\033[2J\033[1;1H";
-    displayMaze(maze, mazeHeight, mazeWidth);
+    dfsSolve(maze, mazeHeight, mazeWidth, findStartPosition(maze, mazeHeight, mazeWidth));
+    std::cout << "The maze was solved yea";
     return 0;
 }
