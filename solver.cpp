de9e5d9bb2bc598deb4aa31be72d1a49270e6786
kjellden <kjelldenknutzen@gmail.com>
2024-06-12 18:29:18 -0600

    Noticed my dfs looked sloppy and had alot of bad code so I did it again this time better -- I would like to add a few things to make it faster though.

--- solver.cpp
+++ solver.cpp
@@ -30,53 +30,37 @@ std::pair<int, int> findStartPosition(char** maze, int rows, int cols) {
 }
 
 // solver the maze using dfs
-bool dfsSolve(char** maze,int mazeHeight, int mazeWidth, pair<int,int> pos){
+bool dfsSolve(char** maze,int mazeHeight, int mazeWidth, int x, int y){
     pair<int, int> move;
     bool solved;
-    
-    displayMaze(maze, mazeHeight, mazeWidth);
-    Sleep(500);
+    if (maze[x][y] == 'X' or maze[x][y] == '!'){
+        return false;
+    }
 
-    if (maze[pos.first][pos.second] == 'E'){
+    if (maze[x][y] == 'E'){
         return true;
     }
 
-    maze[pos.first][pos.second] = '*';
+    char temp = maze[x][y];
+    maze[x][y] = '!';
 
-    if (maze[pos.first + 1][pos.second] == ' ' or maze[pos.first + 1][pos.second] == 'E'){
-        move = make_pair(pos.first + 1, pos.second);
-        if (dfsSolve(maze, mazeHeight, mazeWidth, move)){
-            return true;
-        }
+    displayMaze(maze, mazeHeight, mazeWidth);
+    Sleep(500);
+
+     if (dfsSolve(maze, mazeHeight, mazeWidth, x - 1, y) or // Up
+        dfsSolve(maze, mazeHeight, mazeWidth, x + 1, y) or // Down
+        dfsSolve(maze,  mazeHeight, mazeWidth, x, y - 1) or // Left
+        dfsSolve(maze, mazeHeight, mazeWidth, x, y + 1)) { // Right
+        return true;
     }
 
-    if (maze[pos.first][pos.second + 1] == ' ' or maze[pos.first][pos.second + 1] == 'E'){
-        move = make_pair(pos.first, pos.second + 1);
-        if (dfsSolve(maze, mazeHeight, mazeWidth, move)){
-            return true;
-        }
-    }
-
-    if (maze[pos.first - 1][pos.second] == ' ' or maze[pos.first - 1][pos.second] == 'E'){
-        move = make_pair(pos.first - 1, pos.second);
-        if (dfsSolve(maze, mazeHeight, mazeWidth, move)){
-            return true;
-        }
-    }
-
-    if (maze[pos.first][pos.second -1] == ' ' or maze[pos.first][pos.second - 1] == 'E'){
-        move = make_pair(pos.first, pos.second - 1);
-        if (dfsSolve(maze, mazeHeight, mazeWidth, move)){
-            return true;
-        }
-    }
-
-    maze[pos.first][pos.second] = '!';
+    maze[x][y] = temp;
     return false;
 }
 
 int main() {
     // set vars
+    pair<int, int> startPos;
     int mazeSelect;
     char** maze;
     int mazeHeight;
@@ -104,7 +88,8 @@ int main() {
      
     // start displaying maze
     std::cout << "\033[2J\033[1;1H";
-    dfsSolve(maze, mazeHeight, mazeWidth, findStartPosition(maze, mazeHeight, mazeWidth));
+    startPos = findStartPosition(maze, mazeHeight, mazeWidth);
+    dfsSolve(maze, mazeHeight, mazeWidth, startPos.first, startPos.second);
     std::cout << "The maze was solved yea";
     return 0;
 }
